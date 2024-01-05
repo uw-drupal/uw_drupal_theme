0100fbd8bf10082e893a0a43b49186c43fe3a52a
Andy Krikawa <akrikawa@uw.edu>
2024-01-05 14:28:44 -0800

    ensures alignment with uw_wp_theme 1.3.2

--- js/2014/search.js
+++ js/2014/search.js
@@ -13,7 +13,11 @@
     searchSite = searchArea.dataset.search,
     searchBar = document.getElementById("uw-search-bar");
   function switchAction() {
-    "uw" === searchSite ? ((form.action = "https://uw.edu/search"), searchBar.setAttribute("name", "q")) : ((form.action = $("form.uw-search").data("sitesearch")), searchBar.setAttribute("name", "s"));
+    "uw" === searchSite
+      ? ((form.action = "https://uw.edu/search"),
+        searchBar.setAttribute("name", "q"))
+      : ((form.action = $("form.uw-search").data("sitesearch")),
+        searchBar.setAttribute("name", "s"));
   }
   function toggleSearchArea() {
     document.body.classList.toggle("search-open");
@@ -23,25 +27,54 @@
   }
   function toggleBlur() {
     document.body.classList.contains("search-open")
-      ? ((searchArea.hidden = !1), searchBar.focus(), setAttributes(searchButton, { "aria-hidden": "false", "aria-label": "close search area", "aria-expanded": "true", role: "search" }))
-      : ((searchArea.hidden = !0), setAttributes(searchButton, { "aria-hidden": "true", "aria-label": "open search area", "aria-expanded": "false", role: "search" }));
+      ? ((searchArea.hidden = !1),
+        searchBar.focus(),
+        setAttributes(searchButton, {
+          "aria-hidden": "false",
+          "aria-label": "close search area",
+          "aria-expanded": "true",
+          role: "search",
+        }))
+      : ((searchArea.hidden = !0),
+        setAttributes(searchButton, {
+          "aria-hidden": "true",
+          "aria-label": "open search area",
+          "aria-expanded": "false",
+          role: "search",
+        }));
   }
   if (
     ((document.onkeydown = function (e) {
       return e.keyCode !== UW.KEYCODES.TAB
-        ? e.keyCode !== UW.KEYCODES.ESC || !$("body").hasClass("search-open") || (e.stopPropagation(), toggleSearchArea(), searchButton.focus(), toggleBlur(), !1)
-        : (e.target !== submitButton || e.shiftKey || searchLabels.classList.add("focused"),
-          e.target != submitButton && searchLabels.classList.contains("focused") && (searchLabels.classList.remove("focused"), !e.shiftKey) ? (e.stopPropagation(), toggleSearchArea(), searchButton.focus(), toggleBlur(), !1) : void 0);
+        ? e.keyCode !== UW.KEYCODES.ESC ||
+            !$("body").hasClass("search-open") ||
+            (e.stopPropagation(),
+            toggleSearchArea(),
+            searchButton.focus(),
+            toggleBlur(),
+            !1)
+        : (e.target !== submitButton ||
+            e.shiftKey ||
+            searchLabels.classList.add("focused"),
+          e.target != submitButton &&
+          searchLabels.classList.contains("focused") &&
+          (searchLabels.classList.remove("focused"), !e.shiftKey)
+            ? (e.stopPropagation(),
+              toggleSearchArea(),
+              searchButton.focus(),
+              toggleBlur(),
+              !1)
+            : void 0);
     }),
-      searchButton.addEventListener("click", function () {
-        toggleSearchArea(), toggleBlur();
+    searchButton.addEventListener("click", function () {
+      toggleSearchArea(), toggleBlur();
+    }),
+    submitButton.addEventListener("click", function () {
+      Array.from(radiobtn).forEach(function (e) {
+        e.disabled = !0;
       }),
-      submitButton.addEventListener("click", function () {
-        Array.from(radiobtn).forEach(function (e) {
-          e.disabled = !0;
-        }),
-          submitForm();
-      }),
+        submitForm();
+    }),
     "none" == window.getComputedStyle(mobileSelect).display)
   )
     for (
@@ -63,4 +96,4 @@
   function submitForm(e) {
     return form.submit(), !1;
   }
-}(Drupal, jQuery));
+})(Drupal, jQuery);
