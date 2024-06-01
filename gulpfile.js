e5bd8e55d9d65011b22a0da9efdc87d430b09085
Andy Krikawa <akrikawa@uw.edu>
2024-05-31 17:03:33 -0700

    Changes to gulp processing. CSS aligns with uw_wp_theme 2.1.0.

--- gulpfile.js
+++ gulpfile.js
@@ -56,35 +56,37 @@ const paths = {
   }
 }
 
+const autoprefixerOptions = {
+  browsers: [
+    'Chrome >= 35',
+    'Firefox >= 38',
+    'Edge >= 12',
+    'Explorer >= 10',
+    'iOS >= 8',
+    'Safari >= 8',
+    'Android 2.3',
+    'Android >= 4',
+    'Opera >= 12']
+};
+
 // Compile sass into CSS & auto-inject into browsers
 function styles () {
   return gulp.src([paths.scss.src])
-    // .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sass({
       includePaths: [
         // './node_modules/bootstrap/scss',
         // '../../contrib/bootstrap_barrio/scss'
       ]
     }).on('error', sass.logError))
-    // .pipe($.postcss())
-    .pipe(postcss([autoprefixer({
-      browsers: [
-        'Chrome >= 35',
-        'Firefox >= 38',
-        'Edge >= 12',
-        'Explorer >= 10',
-        'iOS >= 8',
-        'Safari >= 8',
-        'Android 2.3',
-        'Android >= 4',
-        'Opera >= 12']
-    })]))
-    // .pipe(sourcemaps.write())
+    .pipe(postcss([autoprefixer(autoprefixerOptions)]))
+    .pipe(cleanCss({
+      format: 'beautify' // formats output in a really nice way
+    }))
+    .pipe(sourcemaps.write('./'))
+    // .pipe(cleanCss())
     .pipe(gulp.dest(paths.scss.dest))
-    .pipe(cleanCss())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.scss.dest))
-    .pipe(browserSync.stream())
+    // .pipe(browserSync.stream())
 }
 
 // Copy the javascript files into our js folder
