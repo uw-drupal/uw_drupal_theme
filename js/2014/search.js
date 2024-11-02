7fd2c92306082e8abb80506ffa7516da05d4c4e6
Andy Krikawa <akrikawa@uw.edu>
2024-11-01 17:23:05 -0700

    WIP commit. Starts impelentation of uw_wp_theme changes to 3.3.0.

--- js/2014/search.js
+++ js/2014/search.js
@@ -5,6 +5,7 @@
     searchButton = document.querySelector("button.uw-search"),
     radiobtn = document.getElementsByClassName("radiobtn"),
     mobileSelect = document.getElementById("mobile-search-select"),
+    optionsFieldset = document.getElementById("uw"),
     form = document.querySelector("form.uw-search"),
     searchLabels = document.getElementById("search-labels"),
     submitButton = document.querySelector("input.search"),
@@ -24,43 +25,32 @@
   }
   function toggleBlur() {
     document.body.classList.contains("search-open")
-      ? ((searchArea.hidden = !1), searchBar.focus(), setAttributes(searchButton, { "aria-hidden": "false", "aria-label": "close search area", "aria-expanded": "true", role: "search" }), setAttributes(searchArea, {"aria-hidden": "undefined"}))
-      : ((searchArea.hidden = !0), setAttributes(searchButton, { "aria-hidden": "true", "aria-label": "open search area", "aria-expanded": "false", role: "search" }), setAttributes(searchArea, {"aria-hidden": "true"}));
+      ? (searchArea.hidden = !1, optionsFieldset || mobileSelect ? "none" == window.getComputedStyle(mobileSelect).display ? optionsFieldset.focus() : mobileSelect.focus() : searchBar.focus(), setAttributes(searchButton, {
+        "aria-label": "close search area",
+        "aria-expanded": "true"
+      }), setAttributes(searchArea, {"aria-hidden": "false"})) : (searchArea.hidden = !0, setAttributes(searchButton, {
+        "aria-label": "open search area",
+        "aria-expanded": "false"
+      }), setAttributes(searchArea, {"aria-hidden": "true"}))
   }
-  if (
-    ((document.onkeydown = function (e) {
-      return e.keyCode !== UW.KEYCODES.TAB
-        ? e.keyCode !== UW.KEYCODES.ESC || !$("body").hasClass("search-open") || (e.stopPropagation(), toggleSearchArea(), searchButton.focus(), toggleBlur(), !1)
-        : (e.target !== submitButton || e.shiftKey || searchLabels.classList.add("focused"),
-          e.target != submitButton && searchLabels.classList.contains("focused") && (searchLabels.classList.remove("focused"), !e.shiftKey) ? (e.stopPropagation(), toggleSearchArea(), searchButton.focus(), toggleBlur(), !1) : void 0);
+  if (document.onkeydown = function (e) {
+    return e.keyCode !== UW.KEYCODES.TAB ? e.keyCode !== UW.KEYCODES.ESC || !$("body").hasClass("search-open") || (e.stopPropagation(), toggleSearchArea(), searchButton.focus(), toggleBlur(), !1) : (e.target !== submitButton || e.shiftKey || searchLabels.classList.add("focused"), e.target != submitButton && searchLabels.classList.contains("focused") && (searchLabels.classList.remove("focused"), !e.shiftKey) ? (e.stopPropagation(), toggleSearchArea(), searchButton.focus(), toggleBlur(), !1) : void 0)
+  }, searchButton.addEventListener("click", function () {
+    toggleSearchArea(), toggleBlur()
+  }), submitButton.addEventListener("click", function () {
+    Array.from(radiobtn).forEach(function (e) {
+      e.disabled = !0
     }),
-      searchButton.addEventListener("click", function () {
-        toggleSearchArea(), toggleBlur();
-      }),
-      submitButton.addEventListener("click", function () {
-        Array.from(radiobtn).forEach(function (e) {
-          e.disabled = !0;
-        }),
-          submitForm();
-      }),
-    "none" == window.getComputedStyle(mobileSelect).display)
-  )
-    for (
-      var _loop = function () {
-          var e = radiobtn[i];
-          e.onchange = function () {
-            (searchSite = e.value), switchAction();
-          };
-        },
-        i = 0;
-      i < radioLabel.length;
-      i++
-    )
-      _loop();
-  else
-    mobileSelect.addEventListener("change", function () {
-      (searchSite = mobileSelect.value), switchAction();
-    });
+      submitForm()
+      }),optionsFieldset || mobileSelect) if ("none" == window.getComputedStyle(mobileSelect).display) for (var _loop = function () {
+    var e = radiobtn[i];
+    e.onchange = function () {
+      searchSite = e.value, switchAction()
+    }
+  }, i = 0; i < radioLabel.length; i++) _loop(); else mobileSelect.addEventListener("change", function () {
+    searchSite = mobileSelect.value, switchAction()
+  });
+
   function submitForm(e) {
     return form.submit(), !1;
   }
